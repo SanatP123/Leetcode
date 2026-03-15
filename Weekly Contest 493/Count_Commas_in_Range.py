019ec2c9bfad7990f896b2253e81ae8eb29a4c49
SanatP123 <66819450+SanatP123@users.noreply.github.com>
2026-03-15 07:03:20 +0000

    Create Count_Commas_in_Range.py

---
+++ Weekly Contest 493/Count_Commas_in_Range.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countCommas(self, n: int) -> int:
+        total_commas = 0
+        divisor = 1000
+        while n >= divisor:
+            # Numbers from divisor..n contribute one comma at this level
+            total_commas += (n - divisor + 1)
+
+            # Move to next comma level
+            divisor *= 1000
+        return total_commas
+
+
+sol = Solution()
+inputs = [1002, 998, 1000]
+for s in inputs:
+    print(f"Input: s = \"{s}\"")
+    print(f"Output: {sol.countCommas(s)}\n")
